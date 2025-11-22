6a438e2672fc923ea6f02afd963d3fc5ba4c185e
Le Manh Tuong <lemanhtuong2109@gmail.com>
2025-11-22 18:06:36 +0700

    update

--- backend/bookingmovieticketapp/resources/views/layouts/app.blade.php
+++ backend/bookingmovieticketapp/resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Cinestar</title>
-    <link rel="icon" type="image/png" href="{{ asset('images/Galaxy_Cinema.webp') }}">
+    <link rel="icon" type="image/png" href="{{ asset('images/CineJoy.png') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('css/header.css') }}">
